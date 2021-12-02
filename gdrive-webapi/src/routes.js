a94678bd07e608befc3ee50338b6bf8bf734f4b9
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-02 13:41:02 -0300

    webapi: implementando a rota post

--- gdrive-webapi/src/routes.js
+++ gdrive-webapi/src/routes.js
@@ -1,7 +1,9 @@
 import FileHelper from "./fileHelper.js"
 import { logger } from "./logger.js"
 import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath, parse } from "url";
+import UploadHandler from "./uploadHandler.js";
+import { pipeline } from "stream/promises";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const defaultDownloadsFolder = resolve(__dirname, '../', 'downloads')
@@ -28,8 +30,29 @@ export default class Routes {
    }
 
    async post(request, response) {
-      logger.info('ae')
-      response.end()
+      const { headers } = request
+
+      const { query: { socketId } } = parse(request.url, true)
+      const uploadHandler = new UploadHandler({
+         socketId,
+         io: this.io,
+         downloadsFolder: this.downloadsFolder
+      })
+
+      const onFinish = request => () => {
+         request.writeHead(200)
+         const data = JSON.stringify({ result: 'Files uploaded with success!' })
+         response.end(data)
+      }
+
+      const busboyInstance = uploadHandler.registerEvents(headers, onFinish(response))
+
+      await pipeline(
+         request,
+         busboyInstance
+      )
+
+      logger.info('Request finished with success!!')
    }
 
    async get(request, response) {
