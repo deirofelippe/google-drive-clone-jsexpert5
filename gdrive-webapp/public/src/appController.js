bb776c51c1337c98372da772e899aa86b415499b
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-03 05:26:03 -0300

    webapp: upload dos arquivos pro backend

--- gdrive-webapp/public/src/appController.js
+++ gdrive-webapp/public/src/appController.js
@@ -2,10 +2,26 @@ export default class AppController {
    constructor({ connectionManager, viewManager }) {
       this.connectionManager = connectionManager
       this.viewManager = viewManager
+
+      this.uploadingFiles = new Map()
    }
 
    async initialize() {
       this.viewManager.configureFileBtnClick()
+      this.viewManager.configureOnFileChange(this.onFileChange.bind(this))
+      this.connectionManager.configureEvents(() => { })
+
+      await this.updateCurrentFiles()
+   }
+
+   async onFileChange(files) {
+      const requests = []
+      for (const file of files) {
+         this.uploadingFiles.set(file.name, file)
+         requests.push(this.connectionManager.uploadFile(file))
+      }
+
+      await Promise.all(requests)
       await this.updateCurrentFiles()
    }
 
