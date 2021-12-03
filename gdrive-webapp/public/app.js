c0cbbdc3e78617cfa1a9710284a0fbcf62000c40
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-03 03:55:41 -0300

    webapp: arquivo principal

--- gdrive-webapp/public/app.js
+++ gdrive-webapp/public/app.js
@@ -1,1 +1,18 @@
-console.debug('hello world!')
+import AppController from "./src/appController.js";
+import ConnectionManager from "./src/connectionManager.js";
+import ViewManager from "./src/viewManager.js";
+
+const API_URL = "https://0.0.0.0:3000"
+
+const appController = new AppController({
+   viewManager: new ViewManager(),
+   connectionManager: new ConnectionManager({
+      apiUrl: API_URL
+   })
+})
+
+try {
+   await appController.initialize()
+} catch (error) {
+   console.error('error on initializing', error);
+}
